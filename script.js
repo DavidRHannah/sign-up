b4617346f107286c78354d15f8e9185eb12bcff0
David Hannah <dhann013@ucr.edu>
2024-09-11 04:54:32 -0700

    add icons, sticky navbar, theme toggle

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    document.documentElement.setAttribute(
+        'data-theme',
+        document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark'
+    );
+});
